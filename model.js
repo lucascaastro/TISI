ee685ad561f08fc512df881f6f9b215b0e1b6e62
Guilherme Mello <gmello@debian>
2016-10-09 18:53:33 -0300

    Limpeza Geral, CRUD Usuario

--- model.js
+++ model.js
@@ -6,45 +6,17 @@ var bodyParser = require('body-parser');
 
 // cria aplicação express
 var app = express();
+app.listen(3000)
 
 // cria objeto capaz de manipular documento JSON
 var jsonParser = bodyParser.json();
 
 // cria um parser application/x-www-form-urlencoded
 var urlencodedParser = bodyParser.urlencoded({extended:true})
-<<<<<<< HEAD
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('postgres://root:92528109@localhost:5432/TISI')
-//var sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
-
-//banco de dados 
-var User = sequelize.define('tb_usuarios', {
-	nome:{
-		type: Sequelize.STRING	
-	},
-	sobrenome: Sequelize.STRING
-});
-User.sync({force:true})
-	.then(function(){
-		nome:'Lucas',
-		sobrenome:'Castro'
-	});
-});
-=======
 
 var pg = require('pg');
->>>>>>> cc2045f82c1d6cd69d76dbdda653789ae4ded4fe
 
-app.listen(3000)
 
-<<<<<<< HEAD
-// rota via GET 
-app.get('/', function (req, res) {                                  
-   res.send('<html><body> id: '+ req.param('id') + '<br/> Nome Projeto: ' + req.param('nomeprojeto') +  '<br/>' + 
-	   'Usuario: ' +  req.param('usuario') + '<br/>' + 'Data Inicio: ' + req.param('data_entregainicio') + '<br/> ' + 
-	   'Data Entrega: ' + req.param('data_entrega') + '<br/>' + 'Data Termino: ' + req.param('data_termino') + '</body></html>')
- }) 
-=======
 var config = {
   user: 'postgres', //env var: PGUSER 
   database: 'TISI', //env var: PGDATABASE 
@@ -53,7 +25,6 @@ var config = {
   max: 10, // max number of clients in the pool 
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
 };
->>>>>>> cc2045f82c1d6cd69d76dbdda653789ae4ded4fe
 
 // Configure the pool
 var pool = new pg.Pool(config);
@@ -119,7 +90,8 @@ app.get('/retrieveProjeto', function(req, res) {
 
 });
 
-app.post('/updateProjeto', urlencodedParser, function(req, res) {
+// Update
+app.put('/updateProjeto', urlencodedParser, function(req, res) {
 
 
     var id = req.body.id_projeto;
@@ -149,6 +121,7 @@ app.post('/updateProjeto', urlencodedParser, function(req, res) {
 });
 
 
+// Delete
 app.post('/deleteProjeto', urlencodedParser, function(req, res) {
 
 
@@ -180,13 +153,7 @@ app.post('/deleteProjeto', urlencodedParser, function(req, res) {
      });
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-app.listen(3000)
-=======
->>>>>>> cc2045f82c1d6cd69d76dbdda653789ae4ded4fe
-=======
 // CRUD Tarefas
 // Create 
 app.post('/createTarefa', urlencodedParser, function (req, res) {
@@ -247,7 +214,8 @@ app.get('/retrieveTarefa', function(req, res) {
 
 });
 
-app.post('/updateTarefa', urlencodedParser, function(req, res) {
+// Update
+app.put('/updateTarefa', urlencodedParser, function(req, res) {
 
 
     var id = req.body.id_tarefa;
@@ -277,6 +245,7 @@ app.post('/updateTarefa', urlencodedParser, function(req, res) {
 });
 
 
+// Delete
 app.post('/deleteTarefa', urlencodedParser, function(req, res) {
 
     var id = req.body.id_tarefa;
@@ -366,7 +335,8 @@ app.get('/retrieveSubTarefa', function(req, res) {
 
 });
 
-app.post('/updateSubTarefa', urlencodedParser, function(req, res) {
+// Update
+app.put('/updateSubTarefa', urlencodedParser, function(req, res) {
 
 
     var id = req.body.id_subtarefa;
@@ -397,12 +367,10 @@ app.post('/updateSubTarefa', urlencodedParser, function(req, res) {
       });
 });
 
-
+// Delete
 app.post('/deleteSubTarefa', urlencodedParser, function(req, res) {
 
     var id = req.body.id_subtarefa;
-
-    var id = req.body.id_projeto;
 
     // Get a Postgres client from the connection pool
     pool.connect(function(err, client, done) {
@@ -429,4 +397,126 @@ app.post('/deleteSubTarefa', urlencodedParser, function(req, res) {
      });
 });
 
->>>>>>> e04d616cce06a1cb93e34a538cc5660edf13e752
+
+//CRUD Uusuario 
+// Create 
+app.post('/createUsuario', urlencodedParser, function (req, res) {
+	
+	  var data = { nome: req.body.nome, sobrenome: req.body.sobrenome, 
+		      username: req.body.username, senha: req.body.senha, fg_ativo: req.body.fg_ativo};
+
+	  pool.connect(function(err, client, done) {
+
+	  if(err) {
+	    return console.error('error fetching client from pool', err);
+	  }
+
+	  client.query( "INSERT INTO tb_usuarios( nome, sobrenome, username, senha, fg_ativo ) values($1,$2,$3,$4,$5)", 
+			[ data.nome, data.sobrenome, data.username, data.senha, data.fg_ativo ])
+
+	  done();
+
+	  if(err) {
+	    return console.error('error running query', err);
+	  }
+
+  });
+});
+
+// Retrieve
+app.get('/retrieveUsuario', function(req, res) {
+
+	var saida = "<table border=\"1\">";
+
+    pool.connect(function(err, client, done) {
+
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+         client.query('SELECT * FROM tb_usuarios ORDER BY id_usuario ASC', function(err,result){
+
+	   done();
+
+	   if(err){
+		return console.error('error running query', err);
+	   }
+
+	   for( i=0; i < result.rows.length; i++){
+		saida = saida + "<tr><td>" + result.rows[i].id_usuario + "</td><td>"+ result.rows[i].nome + "</td><td>" + result.rows[i].sobrenome + "</td><td" + result.rows[i].username + "</td><td> " + result.rows[i].senha + "</td></tr>";
+	   }
+
+	   saida = saida + "</table>";
+	   res.send(saida);
+
+        });
+
+    });
+
+});
+
+// Update
+app.put('/updateUsuario', urlencodedParser, function(req, res) {
+
+
+    var id = req.body.id_usuario;
+
+    var data = { nome: req.body.nome, sobrenome: req.body.sobrenome, username: req.body.username, senha:req.body.senha };
+
+
+    // Get a Postgres client from the connection pool
+    pool.connect(function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).send(json({ success: false, data: err}));
+        }
+
+	client.query('update tb_usuarios set nome = \'' + data.nome + '\', sobrenome = \'' + data.sobrenome + '\', username = \'' + data.username+ '\', senha= \'' + data.senha + '\'  where id_usuario = ' + id,  
+	  function(err, result){
+
+	  done();
+
+	  if(err){
+		return console.error('error running query', err);
+	  } 
+
+	  res.send("Usuario atualizado");
+	});
+      });
+});
+
+// Delete
+app.post('/deleteUsuario', urlencodedParser, function(req, res) {
+
+    var id = req.body.id_usuario;
+
+
+    // Get a Postgres client from the connection pool
+    pool.connect(function(err, client, done) {
+
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        client.query('DELETE FROM tb_usuarios WHERE id_usuario = ' + id, function(err, result) {
+
+	  done();
+
+	  if( err ){
+		return console.error('error running query', err);
+	  }
+
+	  res.send("Subtarefa removida com sucesso");
+
+	});
+
+     });
+});
+
